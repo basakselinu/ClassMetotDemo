c30e39bb094f6d3a21f34b5f2f946d3aab4eef90
basak <basak@BASAKSELIN>
2024-09-19 20:09:36 +0300

    KodlamaIO - Ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("{0} Müşteri Numarası İle {1} İsimli Müşteri Sisteme Eklendi.", musteri.MusteriNumarasi,musteri.Ad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} Müşteri Numarasına Sahip {1} İsimli Müşteri Sistemden Silindi.", musteri.MusteriNumarasi, musteri.Ad);
+
+        }
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine("{0} Müşteri Numarasına Sahip {1} İsimli Müşterinin Bilgileri Başarıyla Güncellendi.", musteri.MusteriNumarasi,musteri.Ad);
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(
+                     "\nMüşterinin Adı: " + musteri.Ad +
+                     "\nMüşterinin SoyAdı: " + musteri.SoyAd +
+                     "\nMüşterinin Yaşı: " + musteri.Yas +
+                     "\nMüşteri Numarası: " + musteri.MusteriNumarasi
+                     );
+            }
+
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Başak Selin";
+            musteri1.SoyAd = "Ünsal";
+            musteri1.Yas = 24;
+            musteri1.MusteriNumarasi = "4538";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Yaren";
+            musteri2.SoyAd = "İlikkan";
+            musteri2.Yas = 36;
+            musteri2.MusteriNumarasi = "6921";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Batuhan";
+            musteri3.SoyAd = "Yılmaz";
+            musteri3.Yas = 24;
+            musteri3.MusteriNumarasi = "7953";
+
+            Musteri[] musleriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Güncelle(musteri3);
+
+            Console.WriteLine("\n---------- MÜŞTERİ BİLGİLERİ ---------");
+
+            musteriManager.Listele(musleriler);
+
+
+        }
+    }
+}
+
+//do not repeat yourself - DRY - Clean Code - Best Practice
